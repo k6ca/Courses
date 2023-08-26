c8abfd669543dd30d065ad66d660d1451b747a7c
EDUARDO DOMINGOS VIEIRA <eduardo_d_vieira@estudante.sesisenai.org.br>
2023-08-25 21:21:37 -0300

    Exercicios do/while

--- Entra21/20230823/ex8.js
+++ Entra21/20230823/ex8.js
@@ -1,3 +1,18 @@
 /*8) Na maioria das linguagens de programação, é possível contar caracteres de uma string (palavra, 
     frase, etc). Criar um sistema no qual o usuário digite 5 palavras e ao final mostre o tamanho em 
     número de caracteres, de cada uma delas. (Pesquisar sobre o comando length, para solução).*/
+
+let palavra, palavraS = ""
+let entrada = 1
+let tamanhoPalavra = 0
+
+
+while(entrada !== 6){
+    palavra = prompt("Entre com a palavra: ")
+    alert(`A palavra ${palavra}, possui ${parseInt(palavra.length)} caracteres`)
+    tamanhoPalavra = tamanhoPalavra + parseInt(palavra.length)
+    palavraS = palavraS + palavra.toUpperCase() + ", "
+    entrada++
+}
+
+alert(`As palavras digitadas foram ${palavraS}, e o total de caracteres digitados foi ${tamanhoPalavra}.`)
