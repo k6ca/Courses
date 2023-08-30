b938d935c893179782cebee2f361814837cdf9ce
EDUARDO DOMINGOS VIEIRA <eduardo_d_vieira@estudante.sesisenai.org.br>
2023-08-29 22:10:02 -0300

    20230829 exercicios prontos e enunciados

---
+++ Entra21/20230829/ex6.js
@@ -0,0 +1,5 @@
+/*6) Elaborar um sistema similar a um cofre "porquinho". O cofre aceita moedas de 1 centavo, 5 centavos,
+ 10 centavos, 25 centavos e 50 centavos. O programa deve iniciar perguntando qual moeda deseja inserir 
+ no cofre e repetir o processo até que o usuário digite 0 (condição para encerrar). Após encerrar, 
+ deve-se mostrar a quantidade de cada tipo de moeda que foi inserida naquele dia e o total acumulado de
+  economia.*/
