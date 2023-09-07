2e42e2f6c8e12d1ea81254d118484824e210d2e9
EDUARDO DOMINGOS VIEIRA <eduardo_d_vieira@estudante.sesisenai.org.br>
2023-09-06 21:17:49 -0300

    Exercicio correto

--- Entra21/JS_Avançado/20230830_ex3.js
+++ Entra21/JS_Avançado/20230830_ex3.js
@@ -1,32 +1,36 @@
 /*Crie uma função que determine quantos números primos existem dentro de um
 intervalo que o usuário informar. Utilize uma abordagem de repetição com for ou while.*/
 
+/* SÂO números primos: 2, 3, 5, 7, 11, 13, 17, 19 */
 
-/*Errado*/
+/*Código Correto, porém não pega o número 2*/
 
-let quant = 0, divisor = 2, nPrimo=0, ehPrimo=0
-let numUser = 0
+let quant = 0, nPrimo=0, ehPrimo=0
 let numInicial, numFinal
+let listPrimos = []
 
 numInicial = Number(prompt("Entre com o numero INICIAL do intervalo:"))
 numFinal = Number(prompt("Entre com o numero FINAL do intervalo:")) + 1
 
-while(numInicial !== numFinal){
-        while(divisor < numInicial){
-            divisor++
+function numeroPrimo(num) {
+    for(i=2; i<num; i++) {
+      if(num % i === 0) {
+        nPrimo++
+        break
+      }else{
+        ehPrimo++
+        listPrimos.push(numInicial)
+        break
+      }
+    }
+  }
 
-            if(numInicial % divisor === 0){
-                nPrimo++
-                break
-            }
-            else{
-                ehPrimo++
-                break
-            }
-    
-        }
+while(numInicial <= numFinal){
+    numeroPrimo(numInicial)
     quant++
     numInicial++
     }
 
-    alert(`Foram ${quant} interações, são ${ehPrimo} numeros primos e ${nPrimo} numeros não primos.`)
+alert(`Foram ${quant} interações, são ${ehPrimo} numeros primos e ${nPrimo} numeros não primos./nSegue a lista ${listPrimos}`)
+
+
