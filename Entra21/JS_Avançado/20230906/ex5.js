a4bf96d3bc51dded0e33d6b7392f7c20201391a2
EDUARDO DOMINGOS VIEIRA <eduardo_d_vieira@estudante.sesisenai.org.br>
2023-09-06 22:13:08 -0300

    update

--- Entra21/JS_Avançado/20230906/ex5.js
+++
@@ -1,3 +0,0 @@
-/*Faça um programa que recebe três números do usuário, e identifica o maior através
-de uma função e o menor número através de outra função e depois os exibe por um
-alert.*/
--- Entra21/JS_Avançado/20230906/ex6.js
+++
@@ -1,2 +0,0 @@
-/*Crie uma função que recebe um número inteiro e retorna todos os primos, de 1 até
-ele.*/
