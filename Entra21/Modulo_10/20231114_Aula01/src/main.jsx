9b639aadfc7646c82e6972992356146c041695a5
EDUARDO DOMINGOS VIEIRA <eduardod.vieira@gmail.com>
2023-11-21 19:40:37 -0300

    Mod.10 - Aula01 - Atividade

--- Entra21/Modulo_10/20231114_Aula01/src/main.jsx
+++ Entra21/Modulo_10/20231114_Aula01/src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/Home.jsx'
-
+import { Home } from './pages/Home/Index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <Home />
   </React.StrictMode>,
 )
