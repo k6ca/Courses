aa8ceee7d777bde3c1ea9ff5ca52f88cc646b956
EDUARDO DOMINGOS VIEIRA <eduardod.vieira@gmail.com>
2023-11-21 22:04:32 -0300

    Mod.10 - Aula03

--- Entra21/Modulo_10/20231114_Aula01/src/pages/Home/index.jsx
+++ Entra21/Modulo_10/20231114_Aula01/src/pages/Home/index.jsx
@@ -1,15 +1,24 @@
 import "./styles.css"
-import Imagem from "../../img/main.png"
-import Footer from "../../img/footer.png"
-import Line from "../../img/line.png"
+import Logo from "../../img/logo.png"
+
 
 export function Home(){
     return(
         <>
-            <img src={Imagem} alt="Logo Projeto - Aulas React" />
-            <br/>
-            <img src={Line} alt="Line Projeto - Aulas React" />
-            <img src={Footer} alt="Footer Projeto - Aulas React" />
+        <main>
+            <header>
+                <img src={Logo} alt="Logo Projeto - Aulas React" />
+            </header>
+
+            <h1>O seu próximo <strong>destino</strong> pode estar aqui...</h1>
+
+            <h2>Aqui você encontra uma seleção de pacotes completos para as suas férias!</h2>
+            
+            <h2>No ConnectFlight você encontra pacotes de viagens nacionais e internacionais que incluem: passagens aéreas, hotéis, café da manhã, e até seguro viagem com preços arrasadores!</h2>
+
+            <h2>Se você ainda não tem um destino definido, confira nossa seção de ofertas.</h2>
+
+        </main>
         </>
     )
 }
---
+++ Entra21/Modulo_10/20231120_Aula02/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.css"
+import Imagem from "../../img/main.png"
+import Footer from "../../img/footer.png"
+import Line from "../../img/line.png"
+
+export function Home(){
+    return(
+        <>
+            <img src={Imagem} alt="Logo Projeto - Aulas React" />
+            <img src={Line} alt="Line Projeto - Aulas React" />
+            <img src={Footer} alt="Footer Projeto - Aulas React" />
+        </>
+    )
+}
