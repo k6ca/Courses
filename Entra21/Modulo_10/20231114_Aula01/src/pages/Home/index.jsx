9cb0565a4afe8253fb1319f6a7a854070b28ebce
k6ca <eduardod.vieira@gmail.com>
2023-12-13 01:52:45 -0300

    Worgking

--- Entra21/Modulo_10/20231114_Aula01/src/pages/Home/index.jsx
+++ Entra21/Modulo_10/20231114_Aula01/src/pages/Home/index.jsx
@@ -10,11 +10,13 @@ export function Home(){
                 <img src={Logo} alt="Logo Projeto - Aulas React" />
             </header>
 
-            <section >
-                <h1>O seu próximo <strong>destino</strong> pode estar aqui...</h1>
-                <h2 class="textoH2">Aqui você encontra uma seleção de <b><strong>pacotes completos</strong></b> para as suas férias!
-                <p>No <em>ConnectFlight</em> você encontra pacotes de viagens nacionais e internacionais que incluem: passagens aéreas, hotéis, café da manhã, e até seguro viagem com <b>preços arrasadores</b>!</p>
-                <p>Se você ainda não tem um destino definido, confira nossa seção de ofertas.</p></h2>
+            <section class="base1">
+                <h1>O seu próximo <strong class="h1">destino</strong> pode estar aqui...</h1>
+                <h2 class="textoH2">Aqui você encontra uma seleção de <strong class="txt1">pacotes completos</strong> para as suas férias!
+                <p>No <strong class="txt2">ConnectFlight</strong> você encontra pacotes de viagens nacionais e internacionais que incluem: passagens aéreas, hotéis, café da manhã, e até seguro viagem com <strong class="txt1">preços arrasadores</strong>!</p></h2>
+            </section>
+            <section class="base2">
+                <h3><p>Se você ainda não tem um destino definido, confira nossa seção de ofertas.</p></h3>
             </section>
 
         </main>
