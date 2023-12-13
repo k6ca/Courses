25e9bfd58b10608724831ca8c3b7c5c4fdffca57
EDUARDO DOMINGOS VIEIRA <eduardo_d_vieira@estudante.sesisenai.org.br>
2023-12-12 22:00:44 -0300

    Revisando React

--- Entra21/Modulo_10/20231114_Aula01/AulaReact/src/main.jsx
+++ Entra21/Modulo_10/20231114_Aula01/AulaReact/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home/index.jsx'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
 )
--- Entra21/Modulo_10/20231114_Aula01/src/pages/Home/index.jsx
+++ Entra21/Modulo_10/20231114_Aula01/src/pages/Home/index.jsx
@@ -9,14 +9,14 @@ export function Home(){
             <header>
                 <img src={Logo} alt="Logo Projeto - Aulas React" />
             </header>
-            <section>
-                <h1>O seu próximo <strong>destino</strong> pode estar aqui...</h1>
 
-                <h2>Aqui você encontra uma seleção de <b>pacotes completos</b> para as suas férias!
+            <section >
+                <h1>O seu próximo <strong>destino</strong> pode estar aqui...</h1>
+                <h2 class="textoH2">Aqui você encontra uma seleção de <b><strong>pacotes completos</strong></b> para as suas férias!
                 <p>No <em>ConnectFlight</em> você encontra pacotes de viagens nacionais e internacionais que incluem: passagens aéreas, hotéis, café da manhã, e até seguro viagem com <b>preços arrasadores</b>!</p>
                 <p>Se você ainda não tem um destino definido, confira nossa seção de ofertas.</p></h2>
-
             </section>
+
         </main>
         </>
     )
